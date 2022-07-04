518c956d16d9aa3ee15173788231d33670dcb960
Igor Bubelov <igor@bubelov.com>
2022-07-04 16:51:33 +0700

    Connect to Tor only when necessary

--- app/src/main/kotlin/tor/TorController.kt
+++ app/src/main/kotlin/tor/TorController.kt
@@ -9,22 +9,36 @@ import android.content.IntentFilter
 import android.content.ServiceConnection
 import android.os.IBinder
 import android.util.Log
+import conf.ConfRepo
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import org.koin.core.annotation.Single
 import org.torproject.jni.TorService
 
 @Single
 class TorController(
-    app: Application,
+    private val app: Application,
+    private val confRepo: ConfRepo,
 ) {
 
     private val _connectionStatus = MutableStateFlow("")
     val connectionStatus = _connectionStatus.asStateFlow()
 
-    init {
-        Log.d("tor", "Init")
+    private val serviceConnection: ServiceConnection by lazy {
+        object : ServiceConnection {
+            override fun onServiceConnected(name: ComponentName, service: IBinder) {}
+            override fun onServiceDisconnected(name: ComponentName) {}
+        }
+    }
 
+    private var bound = false
+
+    init {
         app.registerReceiver(
             object : BroadcastReceiver() {
                 override fun onReceive(context: Context, intent: Intent) {
@@ -36,15 +50,29 @@ class TorController(
             IntentFilter(TorService.ACTION_STATUS),
         )
 
-        app.bindService(
-            Intent(app, TorService::class.java),
-            SilentServiceConnection(),
-            Application.BIND_AUTO_CREATE,
-        )
+        confRepo.load()
+            .map { it.serverUrl }
+            .distinctUntilChanged()
+            .onEach { onServerUrlChanged(it) }
+            .launchIn(GlobalScope)
     }
 
-    private class SilentServiceConnection : ServiceConnection {
-        override fun onServiceConnected(name: ComponentName, service: IBinder) {}
-        override fun onServiceDisconnected(name: ComponentName) {}
+    private fun onServerUrlChanged(url: String) {
+        if (url.contains(".onion:")) {
+            if (!bound) {
+                app.bindService(
+                    Intent(app, TorService::class.java),
+                    serviceConnection,
+                    Application.BIND_AUTO_CREATE,
+                )
+
+                bound = true
+            }
+        } else {
+            if (bound) {
+                app.unbindService(serviceConnection)
+                bound = false
+            }
+        }
     }
 }
